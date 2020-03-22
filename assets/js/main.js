2d3ff261e943b213c1c8dce9f609f38932ead65b
Tanvir Ahamed <you@example.com>
2020-03-22 12:29:55 +0600

    Second Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,4 +128,17 @@
  });
  // End Slick Nav
 
+//  Scroll Up
+$.scrollUp({
+    scrollName: 'scrollUp', // Element ID
+    topDistance: '300', // Distance from top before showing element (px)
+    topSpeed: 300, // Speed back to top (ms)
+    animation: 'fade', // Fade, slide, none
+    animationInSpeed: 200, // Animation in speed (ms)
+    animationOutSpeed: 200, // Animation out speed (ms)
+    scrollText: '<i class="fa fa-angle-up"></i>', // Text for element
+    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+//   Scroll Up
+
 })(jQuery);
